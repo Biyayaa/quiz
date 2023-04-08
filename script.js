5e20c04f1bc3c2cb8a119b8fefbdab71e2cb3f10
Biyayaa <onaleye.ap@gmail.com>
2023-04-08 10:36:19 +0100

    added submitQuiz function to calculate correct responses and alert score

--- script.js
+++ script.js
@@ -1,91 +1,266 @@
-let displayQuestion = document.getElementById('displayQuestion');
-        let displayOptions = document.getElementById('displayOptions');
-        let prevButton = document.getElementById('prevButton');
-        let nextButton = document.getElementById('nextButton');
-        let submitButton = document.getElementById('submitButton');
+let displayQuestion = document.getElementById("displayQuestion");
+let displayOptions = document.getElementById("displayOptions");
+let prevButton = document.getElementById("prevButton");
+let nextButton = document.getElementById("nextButton");
+let submitButton = document.getElementById("submitButton");
 
-        let quiz = [
-            {
-                question: "What is my first name?",
-                options: ["Baba", "Biyaya", "Abiola", "Paul"],
-                answer: "Abiola"
-            },
-            {
-                question: "What is my favorite color?",
-                options: ["Red", "Green", "Blue", "Yellow"],
-                answer: "Blue"
-            },
-            {
-                question: "What is the capital of France?",
-                options: ["Paris", "Berlin", "Madrid", "Rome"],
-                answer: "Paris"
-            },
-        ];
+let quiz = [
+  {
+    question: "What is my first name?",
+    options: ["Baba", "Biyaya", "Abiola", "Paul"],
+    answer: "Abiola",
+    selected: null,
+  },
+  {
+    question: "What is my favorite color?",
+    options: ["Red", "Green", "Black", "Yellow"],
+    answer: "Black",
+    selected: null,
+  },
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "Berlin", "Madrid", "Rome"],
+    answer: "Paris",
+    selected: null,
+  },
+  {
+    question: "What is the capital of Nigeria?",
+    options: ["Africa", "Lagos", "Togo", "Abuja"],
+    answer: "Abuja",
+    selected: null,
+  },
+  {
+    question: "Who is the President of Nigeria?",
+    options: ["Bulaba", "Muhammadu Buhari", "Ellu-p", "Atiku Abubakar"],
+    answer: "Muhammadu Buhari",
+    selected: null,
+  },
+];
 
-        let currentQuestionIndex = 0;
+let currentQuestionIndex = 0;
 
-        function displayQuiz() {
-            displayQuestion.innerHTML = quiz[currentQuestionIndex].question;
+let responses = [];
+console.log(responses);
 
-            for (let j = 0; j < quiz[currentQuestionIndex].options.length; j++) {
-                let option = document.createElement("input");
-                option.type = "radio";
-                option.name = "question" + currentQuestionIndex;
-                option.value = quiz[currentQuestionIndex].options[j];
+function displayQuiz() {
+  displayQuestion.innerHTML = quiz[currentQuestionIndex].question;
 
-                let label = document.createElement("label");
-                label.innerHTML = quiz[currentQuestionIndex].options[j];
+  for (let j = 0; j < quiz[currentQuestionIndex].options.length; j++) {
+    let option = document.createElement("input");
+    option.type = "radio";
+    option.name = "question" + currentQuestionIndex;
+    option.value = quiz[currentQuestionIndex].options[j];
+    option.onclick = function () {
+      quiz[currentQuestionIndex].selected = this.value;
+      responses[currentQuestionIndex] = this.value;
+    };
 
-                let br = document.createElement("br");
+    let label = document.createElement("label");
+    label.innerHTML = quiz[currentQuestionIndex].options[j];
 
-                displayOptions.appendChild(option);
-                displayOptions.appendChild(label);
-                displayOptions.appendChild(br);
-            }
+    let br = document.createElement("br");
 
-            if (currentQuestionIndex === 0) {
-                prevButton.style.display = "none";
-            } else {
-                prevButton.style.display = "inline-block";
-            }
+    displayOptions.appendChild(option);
+    displayOptions.appendChild(label);
+    displayOptions.appendChild(br);
+  }
 
-            if (currentQuestionIndex === quiz.length - 1) {
-                nextButton.style.display = "none";
-                submitButton.style.display = "inline-block";
-            } else {
-                nextButton.style.display = "inline-block";
-                submitButton.style.display = "none";
-            }
-        }
+  if (currentQuestionIndex === 0) {
+    prevButton.style.display = "none";
+  } else {
+    prevButton.style.display = "block";
+  }
 
-        function prevQuestion() {
-            displayOptions.innerHTML = "";
-            currentQuestionIndex--;
-            displayQuiz();
-        }
+  if (currentQuestionIndex === quiz.length - 1) {
+    nextButton.style.display = "none";
+    submitButton.style.display = "block";
+  } else {
+    nextButton.style.display = "block";
+    submitButton.style.display = "none";
+  }
+}
 
-        function nextQuestion() {
-            displayOptions.innerHTML = "";
-            currentQuestionIndex++;
-            displayQuiz();
-        }
+function prevQuestion() {
+  displayOptions.innerHTML = "";
+  currentQuestionIndex--;
+  displayQuiz();
 
-        function submitQuiz() {
-            let radios = document.querySelectorAll('input[type="radio"]:checked');
-            let responses = [];
-            let score = 0;
+  let selectedValue = quiz[currentQuestionIndex].selected;
+  if (selectedValue !== null) {
+    let radioButtons = document.getElementsByName(
+      "question" + currentQuestionIndex
+    );
+    for (let i = 0; i < radioButtons.length; i++) {
+      if (radioButtons[i].value === selectedValue) {
+        radioButtons[i].checked = true;
+      }
+    }
+  }
+}
 
-            for (let i = 0; i < radios.length; i++) {
-                responses.push(radios[i].value);
-            }
+function nextQuestion() {
+  displayOptions.innerHTML = "";
+  currentQuestionIndex++;
+  displayQuiz();
 
-            for (let i = 0; i < quiz.length; i++) {
-                if (responses[i] === quiz[i].answer) {
-                    score++;
-                }
-            }
+  let selectedValue = quiz[currentQuestionIndex].selected;
+  if (selectedValue !== null) {
+    let radioButtons = document.getElementsByName(
+      "question" + currentQuestionIndex
+    );
+    for (let i = 0; i < radioButtons.length; i++) {
+      if (radioButtons[i].value === selectedValue) {
+        radioButtons[i].checked = true;
+      }
+    }
+  }
+}
 
-            alert("You scored " + score + " out of " + quiz.length);
-        }
+function submitQuiz() {
+  let score = 0;
+  
+  for (let i = 0; i < responses.length; i++) {
+    if (responses[i] === quiz[i].answer) {
+      score++;
+    }
+  }
 
-        displayQuiz();
+  alert("You scored " + score + " out of " + quiz.length);
+}
+
+displayQuiz();
+
+// let displayQuestion = document.getElementById("displayQuestion");
+// let displayOptions = document.getElementById("displayOptions");
+// let prevButton = document.getElementById("prevButton");
+// let nextButton = document.getElementById("nextButton");
+// let submitButton = document.getElementById("submitButton");
+
+// let quiz = [
+//   {
+//     question: "What is my first name?",
+//     options: ["Baba", "Biyaya", "Abiola", "Paul"],
+//     answer: "Abiola",
+//     selected: null,
+//   },
+//   {
+//     question: "What is my favorite color?",
+//     options: ["Red", "Green", "Black", "Yellow"],
+//     answer: "Black",
+//     selected: null,
+//   },
+//   {
+//     question: "What is the capital of France?",
+//     options: ["Paris", "Berlin", "Madrid", "Rome"],
+//     answer: "Paris",
+//     selected: null,
+//   },
+//   {
+//     question: "What is the capital of Nigeria?",
+//     options: ["Africa", "Lagos", "Togo", "Abuja"],
+//     answer: "Abuja",
+//     selected: null,
+//   },
+//   {
+//     question: "Who is the President of Nigeria?",
+//     options: ["Bulaba", "Muhammadu Buhari", "Ellu-p", "Atiku Abubakar"],
+//     answer: "Muhammadu Buhari",
+//     selected: null,
+//   },
+// ];
+
+// let currentQuestionIndex = 0;
+
+// function displayQuiz() {
+//   displayQuestion.innerHTML = quiz[currentQuestionIndex].question;
+
+//   for (let j = 0; j < quiz[currentQuestionIndex].options.length; j++) {
+//     let option = document.createElement("input");
+//     option.type = "radio";
+//     option.name = "question" + currentQuestionIndex;
+//     option.value = quiz[currentQuestionIndex].options[j];
+//     option.onclick = function () {
+//       quiz[currentQuestionIndex].selected = this.value;
+//     };
+
+//     let label = document.createElement("label");
+//     label.innerHTML = quiz[currentQuestionIndex].options[j];
+
+//     let br = document.createElement("br");
+
+//     displayOptions.appendChild(option);
+//     displayOptions.appendChild(label);
+//     displayOptions.appendChild(br);
+//   }
+
+//   if (currentQuestionIndex === 0) {
+//     prevButton.style.display = "none";
+//   } else {
+//     prevButton.style.display = "block";
+//   }
+
+//   if (currentQuestionIndex === quiz.length - 1) {
+//     nextButton.style.display = "none";
+//     submitButton.style.display = "block";
+//   } else {
+//     nextButton.style.display = "block";
+//     submitButton.style.display = "none";
+//   }
+// }
+
+// function prevQuestion() {
+//   displayOptions.innerHTML = "";
+//   currentQuestionIndex--;
+//   displayQuiz();
+
+//   let selectedValue = quiz[currentQuestionIndex].selected;
+//   if (selectedValue !== null) {
+//     let radioButtons = document.getElementsByName(
+//       "question" + currentQuestionIndex
+//     );
+//     for (let i = 0; i < radioButtons.length; i++) {
+//       if (radioButtons[i].value === selectedValue) {
+//         radioButtons[i].checked = true;
+//       }
+//     }
+//   }
+// }
+
+// function nextQuestion() {
+//   displayOptions.innerHTML = "";
+//   currentQuestionIndex++;
+//   displayQuiz();
+
+//   let selectedValue = quiz[currentQuestionIndex].selected;
+//   if (selectedValue !== null) {
+//     let radioButtons = document.getElementsByName(
+//       "question" + currentQuestionIndex
+//     );
+//     for (let i = 0; i < radioButtons.length; i++) {
+//       if (radioButtons[i].value === selectedValue) {
+//         radioButtons[i].checked = true;
+//       }
+//     }
+//   }
+// }
+
+// function submitQuiz() {
+//   let radios = document.querySelectorAll('input[type="radio"]:checked');
+//   let score = 0;
+//   let responses = [];
+//   console.log(responses);
+
+//   for (let i = 0; i < radios.length; i++) {
+//     responses.push(radios[i].value);
+//   }
+
+//   for (let i = 0; i < responses.length; i++) {
+//     if (responses[i] === quiz[i].answer) {
+//       score++;
+//     }
+//   }
+
+//   alert("You scored " + score + " out of " + quiz.length);
+// }
+
+// displayQuiz();
