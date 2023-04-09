d311f378f926dab07248c7f73d5d8170440d3a24
Biyayaa <onaleye.ap@gmail.com>
2023-04-09 06:24:33 +0100

    added a sumittion confirmation modal

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ let prevButton = document.getElementById("prevButton");
 let nextButton = document.getElementById("nextButton");
 let submitButton = document.getElementById("submitButton");
 
+let modal = document.getElementById("modal");
+let modalText = document.getElementById("modal-text");
+let modalYes = document.getElementById("modal-yes");
+let modalNo = document.getElementById("modal-no");
+
 let quiz = [
   {
     question: "What is my first name?",
@@ -43,7 +48,7 @@ let responses = [];
 console.log(responses);
 
 function displayQuiz() {
-  displayQuestion.innerHTML = quiz[currentQuestionIndex].question;
+  displayQuestion.innerHTML = currentQuestionIndex +1 +"."+ quiz[currentQuestionIndex].question;
   displayOptions.innerHTML = "";
 
   quiz[currentQuestionIndex].options.forEach((option) => {
@@ -102,149 +107,34 @@ function nextQuestion() {
   saveChoice();
 }
 
-function submitQuiz() {
-  let score = 0;
-  for (let i = 0; i < responses.length; i++) {
-    if (responses[i] === quiz[i].answer) {
-      score++;
-    }
-  }
-  alert("You scored " + score + " out of " + quiz.length);
+function showModal() {
+  let answeredQuestions = responses.filter((response) => response !== undefined).length;
+  let totalQuestions = quiz.length;
+  modalText.innerHTML = `You have answered ${answeredQuestions} out of ${totalQuestions} questions. Are you sure you want to submit?`;
+  modal.style.display = "block";
 }
 
+function hideModal() {
+  modal.style.display = "none";
+}
+
+function submitQuiz() {
+  showModal();
+}
+
+modalYes.onclick = function () {
+  let score = 0;
+  responses.forEach((response, index) => {
+    if (response === quiz[index].answer) {
+      score++;
+    }
+  });
+  alert("You scored " + score + " out of " + quiz.length);
+  hideModal();
+};
+
+modalNo.onclick = function () {
+  hideModal();
+};
+
 displayQuiz();
-
-// let displayQuestion = document.getElementById("displayQuestion");
-// let displayOptions = document.getElementById("displayOptions");
-// let prevButton = document.getElementById("prevButton");
-// let nextButton = document.getElementById("nextButton");
-// let submitButton = document.getElementById("submitButton");
-
-// let quiz = [
-//   {
-//     question: "What is my first name?",
-//     options: ["Baba", "Biyaya", "Abiola", "Paul"],
-//     answer: "Abiola",
-//     selected: null,
-//   },
-//   {
-//     question: "What is my favorite color?",
-//     options: ["Red", "Green", "Black", "Yellow"],
-//     answer: "Black",
-//     selected: null,
-//   },
-//   {
-//     question: "What is the capital of France?",
-//     options: ["Paris", "Berlin", "Madrid", "Rome"],
-//     answer: "Paris",
-//     selected: null,
-//   },
-//   {
-//     question: "What is the capital of Nigeria?",
-//     options: ["Africa", "Lagos", "Togo", "Abuja"],
-//     answer: "Abuja",
-//     selected: null,
-//   },
-//   {
-//     question: "Who is the President of Nigeria?",
-//     options: ["Bulaba", "Muhammadu Buhari", "Ellu-p", "Atiku Abubakar"],
-//     answer: "Muhammadu Buhari",
-//     selected: null,
-//   },
-// ];
-
-// let currentQuestionIndex = 0;
-
-// function displayQuiz() {
-//   displayQuestion.innerHTML = quiz[currentQuestionIndex].question;
-
-//   for (let j = 0; j < quiz[currentQuestionIndex].options.length; j++) {
-//     let option = document.createElement("input");
-//     option.type = "radio";
-//     option.name = "question" + currentQuestionIndex;
-//     option.value = quiz[currentQuestionIndex].options[j];
-//     option.onclick = function () {
-//       quiz[currentQuestionIndex].selected = this.value;
-//     };
-
-//     let label = document.createElement("label");
-//     label.innerHTML = quiz[currentQuestionIndex].options[j];
-
-//     let br = document.createElement("br");
-
-//     displayOptions.appendChild(option);
-//     displayOptions.appendChild(label);
-//     displayOptions.appendChild(br);
-//   }
-
-//   if (currentQuestionIndex === 0) {
-//     prevButton.style.display = "none";
-//   } else {
-//     prevButton.style.display = "block";
-//   }
-
-//   if (currentQuestionIndex === quiz.length - 1) {
-//     nextButton.style.display = "none";
-//     submitButton.style.display = "block";
-//   } else {
-//     nextButton.style.display = "block";
-//     submitButton.style.display = "none";
-//   }
-// }
-
-// function prevQuestion() {
-//   displayOptions.innerHTML = "";
-//   currentQuestionIndex--;
-//   displayQuiz();
-
-//   let selectedValue = quiz[currentQuestionIndex].selected;
-//   if (selectedValue !== null) {
-//     let radioButtons = document.getElementsByName(
-//       "question" + currentQuestionIndex
-//     );
-//     for (let i = 0; i < radioButtons.length; i++) {
-//       if (radioButtons[i].value === selectedValue) {
-//         radioButtons[i].checked = true;
-//       }
-//     }
-//   }
-// }
-
-// function nextQuestion() {
-//   displayOptions.innerHTML = "";
-//   currentQuestionIndex++;
-//   displayQuiz();
-
-//   let selectedValue = quiz[currentQuestionIndex].selected;
-//   if (selectedValue !== null) {
-//     let radioButtons = document.getElementsByName(
-//       "question" + currentQuestionIndex
-//     );
-//     for (let i = 0; i < radioButtons.length; i++) {
-//       if (radioButtons[i].value === selectedValue) {
-//         radioButtons[i].checked = true;
-//       }
-//     }
-//   }
-// }
-
-// function submitQuiz() {
-//   let radios = document.querySelectorAll('input[type="radio"]:checked');
-//   let score = 0;
-//   let responses = [];
-//   console.log(responses);
-
-//   for (let i = 0; i < radios.length; i++) {
-//     responses.push(radios[i].value);
-//   }
-
-//   for (let i = 0; i < responses.length; i++) {
-//     if (responses[i] === quiz[i].answer) {
-//       score++;
-//     }
-//   }
-
-//   alert("You scored " + score + " out of " + quiz.length);
-// }
-
-// displayQuiz();
